9096142808ce639ec86396181ea3ef9b6b7d54e0
Administrator <admin@example.com>
2021-04-03 15:56:40 +0530

    online Meet-ups

--- assets/js/main.js
+++ assets/js/main.js
@@ -54,7 +54,7 @@ $(document).ready(function() {
 	
 	/* ======= Countdown ========= */
 	// set the date we're counting down to
-    var target_date = new Date("July 17, 2020").getTime();
+    var target_date = new Date("July 17, 2021").getTime();
      
     // variables for time units
     var days, hours, minutes, seconds;
