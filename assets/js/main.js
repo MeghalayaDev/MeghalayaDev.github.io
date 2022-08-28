2114f6fedf20a3e892e2fbc387e744285e7fcaba
Kyrshanlang R Dkhar <bwidu.peace@gmail.com>
2022-08-28 17:10:56 +0530

    dev.connect 2022

--- assets/js/main.js
+++ assets/js/main.js
@@ -54,7 +54,7 @@ $(document).ready(function() {
 	
 	/* ======= Countdown ========= */
 	// set the date we're counting down to
-    var target_date = new Date("October 8, 2022").getTime();
+    var target_date = new Date("October 22, 2022").getTime();
      
     // variables for time units
     var days, hours, minutes, seconds;
